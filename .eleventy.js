a385078a1aea8e167429eebd068a47c07f45d391
Ben Sweedler <bensweedler@granular.ag>
2020-07-19 12:20:47 -0500

    trying to get images to copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const Video = require(`./_includes/components/Video.js`);
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addShortcode('Video', Video);
 };
